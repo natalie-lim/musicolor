b92c23d315952c22552aea0c751846eceb13236f
Natalie Lim <natalielim1906@gmail.com>
2025-01-22 18:24:28 -0500

    refactored files

---
+++ src/PalettePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./PalettePage.css";
+
+function PalletePage() {
+  return (
+    <div className="profile-page">
+      <h1>pick which playlist</h1>
+      <p>Welcome to the profile page!</p>
+    </div>
+  );
+}
+
+export default PalletePage;
